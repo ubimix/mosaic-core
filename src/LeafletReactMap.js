492f94f880893ca52f12e6c302c0b801236b4590
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-08-28 16:00:31 +0200

    Zoom-specific CSS class names were added to the map container.

--- src/LeafletReactMap.js
+++ src/LeafletReactMap.js
@@ -23,14 +23,17 @@ function(require) {
         componentDidMount : function() {
             var container = this.refs.container.getDOMNode();
             this.map = this._newMap(container);
+            this.map.on('zoomend', this._updateZoomStyles, this);
             if (this.props.onMapAdd) {
                 this.props.onMapAdd(this.map);
             }
+            this._updateZoomStyles();
         },
         componentWillUnmount : function() {
             if (this.props.onMapRemove) {
                 this.props.onMapRemove(this.map);
             }
+            this.map.off('zoomend', this._updateZoomStyles, this);
             this.map.remove();
             delete this.map;
         },
@@ -58,6 +61,18 @@ function(require) {
                 map.setView(center, zoom);
             }
             return map;
+        },
+        _updateZoomStyles : function() {
+            var node = this.getDOMNode();
+            var zoom = this.map.getZoom();
+            var cls = [];
+            for (var i = 0; i <= zoom; i++) {
+                cls.push('zoom-' + i);
+            }
+            var css = node.className;
+            css = css.replace(/zoom-\d+\s*/gim, '');
+            css += ' ' + cls.join(' ');
+            node.className = css;
         }
     });
 
