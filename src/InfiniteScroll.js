27a8b1ce82dc9cb103aa1ef0f52a2880130cf96e
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-09-01 22:01:05 +0200

    Exp re-implementation

--- src/InfiniteScroll.js
+++ src/InfiniteScroll.js
@@ -10,7 +10,16 @@ function(require) {
     var Mosaic = require('mosaic-commons');
     var _ = require('underscore');
     var React = require('react');
-
+    function findPos(container, el) {
+        var x = 0;
+        var y = 0;
+        while (el && el !== container) {
+            x += el.offsetLeft;
+            y += el.offsetTop;
+            el = el.offsetParent;
+        }
+        return [ x, y ];
+    }
     /**
      * This is an "infinite scroll" widget allowing to load elements by their
      * position.
@@ -22,15 +31,26 @@ function(require) {
                     this.items = [];
                     _.extend(this, options);
                 },
-                getRecordHeight : function() {
-                    return this.recordHeight || 30;
+                /** Number of items in one 'page'. */
+                getPageSize : function() {
+                    return this.pageSize || 4;
                 },
-                getFullLength : function() {
-                    return this.fullLength || 1000000;
+                /** Returns average height of each item. */
+                getItemHeight : function() {
+                    return this.itemHeight || 30;
                 },
+                /** Returns the total number of items */
+                getSize : function() {
+                    return this.size;
+                },
+                /**
+                 * Loads the specified number of items starting from the given
+                 * position
+                 */
                 loadItems : function(startIndex, size, callback) {
                     throw new Error('Not implemented');
                 },
+                /** Public method to go to the specified position */
                 goTo : function(pos) {
                     this.emit('updatePos', pos);
                 },
@@ -52,229 +72,146 @@ function(require) {
         _newState : function(options) {
             return _.extend({}, this.state, options);
         },
-        componentDidMount : function() {
-            this._setScrollPos(0);
-        },
         componentWillMount : function(nextProps) {
             this.props.model._addPositionListener(this._positionHandler, this);
+            this._adjustPosition = _.debounce(this._adjustPosition, 5);
             this._setScrollPos = _.debounce(_.bind(this._setScrollPos, this),
-                    50);
-            this._setScrollPos(this.state.scrollPos);
+                    30);
         },
+        componentDidMount : function() {
+            this._setScrollPos(this.state.scrollPos);
+            // this.componentDidUpdate();
+        },
+
         componentWillUnmount : function(nextProps) {
             this.props.model._removePositionListener(this._positionHandler,
                     this);
         },
         componentWillReceiveProps : function(nextProps) {
-            // this.setState(this._newState(nextProps));
-            this._setScrollPos(this.state.scrollPos);
+            console.log('I AM HERE!');
+            this._setScrollPos(this.state.scrollPos, true);
         },
         getInitialState : function() {
-            return this._newState({});
-        },
-        _positionHandler : function(pos) {
-            var idx = pos - this.state.index;
-            var scrollPos;
-            if (idx >= 0 && idx < this.state.items.length) {
-                var children = this.refs.block.getDOMNode();
-                scrollPos = this._findScrollPos(children[idx]);
-            } else {
-                var height = this.props.model.getRecordHeight();
-                scrollPos = pos * height;
-            }
-            var node = this.getDOMNode();
-            this._setScrollPos(scrollPos);
-        },
-        _findScrollPos : function(el) {
-            return this._findPos(el)[1];
-        },
-        _findPos : function(el) {
-            var x = 0;
-            var y = 0;
-            if (el) {
-                var container = this.getDOMNode();
-                while (el && el !== container) {
-                    x += el.offsetLeft;
-                    y += el.offsetTop;
-                    el = el.offsetParent;
-                }
-            }
-            return [ x, y ];
-        },
-        componentDidUpdate : function() {
-            var that = this;
-            var blockNode = that.refs.block.getDOMNode();
-            var blockPos = that._findScrollPos(blockNode);
-
-            var children = blockNode.children || [];
-
-            var idx = that.state.offsetIndex - that.state.index;
-            var node = children[idx];
-            var nodePos = that._findScrollPos(node);
-
-            var container = that.getDOMNode();
-            var step = 1; // Math.max(that.props.recordHeight / 10, 1);
-            // scrollTop = heightBefore + (nodePos - offset - blockPos)
-            var scrollTop = Math.ceil(container.scrollTop / step) * step;
-            var heightBefore = Math.round(scrollTop + blockPos - //
-            nodePos + that.state.offset);
-            var before = that.refs.before.getDOMNode();
-
-            if (before.offsetHeight != heightBefore) {
-                before.style.height = Math.max(0, heightBefore) + 'px';
-            }
-            if (container.scrollTop !== scrollTop) {
-                that._disableUpdates = true;
-                setTimeout(function() {
-                    container.scrollTop = scrollTop;
-                    that._disableUpdates = false;
-                }, 1);
-            }
-        },
-        _setScrollPos : function(scrollPos) {
-            var that = this;
-            // Get the visible range ("window")
-            var model = that.props.model;
-            var node = that.getDOMNode();
-            var windowHeight = node.offsetHeight;
-            var windowStart = scrollPos; // node.scrollTop;
-            var windowEnd = windowStart + windowHeight;
-
-            // Get the current block range
-            var blockStart = 0;
-            var blockEnd = 0;
-            var blockStartIndex = that.state.index || 0;
-            var block = that.refs.block;
-            var blockElements = [];
-            if (block) {
-                var blockNode = block.getDOMNode();
-                blockStart = that._findScrollPos(blockNode);
-                blockEnd = blockStart + blockNode.offsetHeight;
-                blockElements = blockNode.children;
-            }
-
-            // Estimate average height of an item
-            var recordHeight = model.getRecordHeight();
-
-            var startIndex;
-            var length;
-            var offset;
-            var offsetIndex;
-
-            var k = 1.5;
-            if (windowEnd < blockStart || windowStart > blockEnd) {
-                if (windowEnd < blockStart) {
-                    startIndex = Math.floor(windowStart / recordHeight);
-                } else if (windowStart > blockEnd) {
-                    var indexInc = Math.floor((windowStart - blockEnd) / //
-                    recordHeight);
-                    startIndex = blockStartIndex + blockElements.length + //
-                    indexInc;
-                }
-                offset = -(windowStart % recordHeight);
-                length = Math.ceil((windowHeight - offset) / recordHeight) * k;
-                offsetIndex = startIndex;
-            } else {
-                startIndex = blockStartIndex;
-                length = blockElements.length;
-                // Remove not visible items at the beginning
-                for (var i = 0; i < blockElements.length; i++) {
-                    var itemNode = blockElements[i];
-                    var height = itemNode.offsetHeight;
-                    if (blockStart + height >= windowStart) {
-                        break;
-                    }
-                    blockStart += height;
-                    startIndex++;
-                    length--;
-                }
-                offset = blockStart - windowStart;
-
-                // Add nodes at the beginning
-                var delta = Math.ceil(Math.abs(offset) / recordHeight);
-                offsetIndex = startIndex;
-                startIndex = Math.max(0, startIndex - delta);
-                length += delta;
-
-                var minBlockLen = this.props.minBlockSize || 10;
-
-                // Remove not visible items at the end
-                for (var i = blockElements.length - 1; i >= 0; i--) {
-                    var itemNode = blockElements[i];
-                    var height = itemNode.offsetHeight;
-                    if (blockEnd - height - offset <= windowEnd) {
-                        break;
-                    }
-                    blockEnd -= height;
-                    length--;
-                }
-                // Add nodes at the end
-                length += Math.ceil(Math.max(0, windowEnd - blockEnd - offset)
-                        / recordHeight)
-                        * k;
-            }
-            var fullLength = model.getFullLength();
-            if (length >= 0) {
-                length = Math.max(0, //
-                Math.min(fullLength - startIndex, length));
-            }
-            if (offset !== 0) {
-                length++;
-            }
-
-            // Load items
-            var container = that.getDOMNode();
-            model.loadItems(startIndex, length, function(items) {
-                var state = that._newState({
-                    scrollPos : scrollPos,
-                    index : startIndex,
-                    items : items,
-                    offset : offset,
-                    offsetIndex : offsetIndex
-                });
-                that.setState(state);
+            return this._newState({
+                index : 0,
+                length : 0,
+                items : [],
+                scrollPos : 0
             });
         },
-        _onScroll : function(event) {
-            if (this._disableUpdates) {
-                return;
+        _positionHandler : function(pos) {
+            var that = this;
+            var model = that.props.model;
+            var h = model.getItemHeight();
+            var scrollPos = pos * h;
+            that._setScrollPos(scrollPos, true);
+        },
+        _adjustPosition : function() {
+            var that = this;
+            var container = that.getDOMNode();
+            var blockNode = that.refs.block.getDOMNode();
+            var wrapperNode = that.refs.wrapper.getDOMNode();
+
+            var scrollPos = container.scrollTop;
+            var model = that.props.model;
+            var h = model.getItemHeight();
+
+            console.log('[BEFORE]: fullHeight:' + wrapperNode.offsetHeight,
+                    'blockPos:' + findPos(container, blockNode)[1],
+                    'blockHeight:' + blockNode.offsetHeight, 'scrollPos:'
+                            + scrollPos);
+            var size = model.getSize();
+            var fullHeight = size * h;
+            if (wrapperNode.offsetHeight != fullHeight) {
+                console.log('!!!!!');
             }
+            wrapperNode.style.height = fullHeight + 'px';
+
+            var pos = scrollPos - that.state.index * h;
+            var height = that.state.items.length * h;
+            var blockHeight = blockNode.offsetHeight;
+            var blockPos = scrollPos - blockHeight * (pos / height);
+            blockPos = Math.round(blockPos);
+            blockNode.style.top = blockPos + 'px';
+
+            console.log(' [AFTER]: fullHeight:' + fullHeight, 'blockPos:'
+                    + blockPos, 'blockHeight:' + blockHeight, 'scrollPos:'
+                    + scrollPos);
+
+        },
+        componentDidUpdate : function() {
+            // this._adjustPosition();
+        },
+        _getBlockInfo : function(scrollPos) {
+            var that = this;
+            var model = that.props.model;
+            var pageSize = model.getPageSize();
+            var h = model.getItemHeight();
+            var container = that.getDOMNode();
+
+            var windowHeight = container.offsetHeight;
+            var delta = windowHeight / 4;
+
+            var startPos = Math.max(scrollPos - delta, 0);
+            var endPos = scrollPos + windowHeight + delta;
+
+            var startItem = Math.floor(startPos / h);
+            var endItem = Math.ceil((endPos + h - 1) / h);
+
+            var startPage = Math.floor(startItem / pageSize);
+            var endPage = Math.ceil((endItem + pageSize - 1) / pageSize);
+
+            var index = startPage * pageSize;
+            var length = (endPage - startPage) * pageSize;
+            return {
+                index : index,
+                length : length
+            };
+        },
+        _setScrollPos : function(scrollPos, force) {
+            console.log('_setScrollPos', scrollPos, force);
+            var that = this;
+            var params = that._getBlockInfo(scrollPos);
+            if (force || params.index != that.state.index
+                    || params.length != that.state.length) {
+                var model = that.props.model;
+                model.loadItems(params.index, params.length, function(items) {
+                    params.items = items;
+                    that.setState(that._newState(params));
+                });
+            }
+        },
+        _onScroll : function(event) {
             this._setScrollPos(this.getDOMNode().scrollTop);
         },
         render : function() {
+            console.log('render')
             var items = this.state.items || [];
-            var startIndex = (this.state.index || 0);
-            var endIndex = startIndex + items.length;
             var model = this.props.model;
-            var fullLength = model.getFullLength();
-            var recordHeight = model.getRecordHeight();
-            var heightBefore = (startIndex * recordHeight) + 'px';
-            var heightAfter = ''
-                    + (Math.max(0, fullLength - endIndex) * recordHeight)
-                    + 'px';
+
+            var size = model.getSize();
+            var h = model.getItemHeight();
+            var blockPos = (this.state.index || 0) * h;
+            var fullHeight = size * h;
+
             return React.DOM.div({
                 id : this.props.id,
                 className : this.props.className,
                 onScroll : this._onScroll
-            },
-            //
-            React.DOM.div({
-                ref : 'before',
+            }, React.DOM.div({
+                ref : 'wrapper',
                 style : {
-                    height : heightBefore
+                    height : fullHeight + 'px',
+                    position : 'relative',
+                // overflow : 'hidden'
                 }
-            }),
-            //
-            React.DOM.div({
+            }, React.DOM.div({
                 ref : 'block',
-            }, items),
-            //
-            React.DOM.div({
-                ref : 'after',
                 style : {
-                    height : heightAfter
+                    top : blockPos + 'px',
+                    position : 'absolute'
                 }
-            }));
+            }, items)));
         }
     });
 
