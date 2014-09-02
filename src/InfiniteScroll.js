a087be9a342f218eeee0f6944b40cece0b83018a
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-09-02 10:36:47 +0200

    A working version of an infinite scroll.

--- src/InfiniteScroll.js
+++ src/InfiniteScroll.js
@@ -16,136 +16,12 @@ function(require) {
      * position.
      */
     return React.createClass({
-        statics : {
-            Model : Mosaic.Class.extend(Mosaic.Events.prototype, {
-                initialize : function(options) {
-                    this.items = [];
-                    _.extend(this, options);
-                },
-                /** Number of items in one 'page'. */
-                getPageSize : function() {
-                    return this.pageSize || 4;
-                },
-                /** Returns average height of each item. */
-                getItemHeight : function() {
-                    return this.itemHeight || 30;
-                },
-                /** Returns the total number of items */
-                getSize : function() {
-                    return this.size;
-                },
-                /**
-                 * Loads the specified number of items starting from the given
-                 * position
-                 */
-                loadItems : function(startIndex, size, callback) {
-                    throw new Error('Not implemented');
-                },
-            })
-        },
-        _newState : function(options) {
-            return _.extend({
 
-            }, this.state, options);
-        },
-        componentWillMount : function(nextProps) {
-            this._adjustPosition = _.debounce(this._adjustPosition, 5);
-            this._setScrollPos = _.debounce(_.bind(this._setScrollPos, this),
-                    30);
-        },
-        componentDidMount : function() {
-            this._setScrollPos(this.state.scrollPos);
-            // this.componentDidUpdate();
-        },
-
-        componentWillReceiveProps : function(nextProps) {
-            // this._setScrollPos(0, true);
-            // this.setState(this._newState());
-            this._setScrollPos(this.state.scrollPos, true);
-        },
-        getInitialState : function() {
-            return this._newState({
-                index : 0,
-                length : 0,
-                items : [],
-                scrollPos : 0
-            });
-        },
-        _adjustPosition : function() {
-            var that = this;
-            var container = that.getDOMNode();
-            var scrollPos = container.scrollTop;
-
-            var model = that.props.model;
-            var h = model.getItemHeight();
-            var pos = scrollPos - that.state.index * h;
-            var height = that.state.items.length * h;
-            var blockNode = that.refs.block.getDOMNode();
-            var blockHeight = blockNode.offsetHeight;
-
-            var blockPos = scrollPos - blockHeight * (pos / height);
-            blockPos = Math.round(blockPos);
-            blockNode.style.top = blockPos + 'px';
-        },
-        componentDidUpdate : function() {
-            this._adjustPosition();
-        },
-        _getBlockInfo : function(scrollPos) {
-            var that = this;
-            var model = that.props.model;
-            var pageSize = model.getPageSize();
-            var h = model.getItemHeight();
-            var container = that.getDOMNode();
-
-            var windowHeight = container.offsetHeight;
-            var delta = windowHeight / 4;
-
-            var startPos = Math.max(scrollPos - delta, 0);
-            var endPos = scrollPos + windowHeight + delta;
-
-            var startItem = Math.floor(startPos / h);
-            var endItem = Math.ceil((endPos + h - 1) / h);
-
-            var startPage = Math.floor(startItem / pageSize);
-            var endPage = Math.ceil((endItem + pageSize - 1) / pageSize);
-
-            // console.log('---------------------------------------------');
-            // console.log('pageSize: ' + pageSize, ' h: ' + h);
-            // console.log('startPos: ' + startPos, 'endPos: ' + endPos);
-            // console.log('startItem: ' + startItem, 'endItem: ' + endItem);
-            // console.log('startPage: ' + startPage, 'endPage: ' + endPage);
-
-            var index = startPage * pageSize;
-            var length = (endPage - startPage) * pageSize;
-            return {
-                scrollPos : scrollPos,
-                index : index,
-                length : length
-            };
-        },
-        _setScrollPos : function(scrollPos, force) {
-            var that = this;
-            var params = that._getBlockInfo(scrollPos);
-            if (force || params.index != that.state.index
-                    || params.length != that.state.length) {
-                var model = that.props.model;
-                model.loadItems(params.index, params.length, function(items) {
-                    params.items = items;
-                    that.setState(that._newState(params));
-                });
-            } else {
-                that._adjustPosition();
-            }
-        },
-        _onScroll : function(event) {
-            this._setScrollPos(this.getDOMNode().scrollTop);
-        },
+        /** The main rendering method. */
         render : function() {
             var items = this.state.items || [];
-            var model = this.props.model;
-
-            var size = model.getSize();
-            var h = model.getItemHeight();
+            var size = this._getSize();
+            var h = this._getItemHeight();
             var blockPos = (this.state.index || 0) * h;
             var fullHeight = size * h;
 
@@ -170,7 +46,176 @@ function(require) {
                     bottom : 'auto'
                 }
             }, items)));
-        }
+        },
+
+        // -------------------------------------------------------------------
+        // React lifecycle methods
+        /**
+         * This method creates "debounced" versions of some methods to avoid to
+         * frequent calls
+         */
+        componentWillMount : function(nextProps) {
+            this._adjustPosition = _.debounce(this._adjustPosition, 5);
+            this._setScrollPos = _.debounce(_.bind(this._setScrollPos, this),
+                    30);
+        },
+        /** Initializes the scroll and set it in an initial position. */
+        componentDidMount : function() {
+            this._resetScrollPos(this.props);
+        },
+        /** Updates the position of the scroller. */
+        componentWillReceiveProps : function(nextProps) {
+            this._resetScrollPos(nextProps);
+        },
+        /**
+         * This method is called after the component is rendered and it adjusts
+         * position of the item block.
+         */
+        componentDidUpdate : function() {
+            this._adjustPosition();
+        },
+        /** Creates and returns the inital state object for this component. */
+        getInitialState : function() {
+            return this._newState({
+                index : 0,
+                length : 0,
+                items : [],
+                scrollPos : 0
+            });
+        },
+
+        // -------------------------------------------------------------------
+        // Internal (private) methods
+
+        /**
+         * This is an internal method creating and returning a new state.
+         */
+        _newState : function(options) {
+            return _.extend({}, this.state, options);
+        },
+        /**
+         * This method returns the size of a "page" - size of block of items
+         * loaded at once. By default this method returns the
+         * "this.props.pageSize" value.
+         */
+        _getPageSize : function() {
+            return this.props.pageSize || 4;
+        },
+        /**
+         * Returns the total number of items to visualize in this scroll.
+         */
+        _getSize : function() {
+            return this.props.length || 0;
+        },
+        /**
+         * Returns an average size of each individual item in the list. This
+         * value is used to calculate the total size of the scroll. By default
+         * this method returns the "this.props.itemHeight" value.
+         */
+        _getItemHeight : function() {
+            return this.props.itemHeight || 10;
+        },
+        /**
+         * @param params.index
+         *            start index of the item to load
+         * @param params.length
+         *            number of items to load
+         * @param params.callback
+         *            a callback method accepting the resulting items
+         */
+        _loadItems : function(params) {
+            this.props.loadItems(params);
+        },
+        /**
+         * Sets the scroll in initial position or in an already existing one if
+         * the initial position is not defined.
+         */
+        _resetScrollPos : function(props) {
+            props = props || this.props;
+            var scrollPos = this.state.scrollPos;
+            var focusedPos = props.focusedIndex;
+            focusedPos = !isNaN(focusedPos) ? focusedPos : -1;
+            var size = this._getSize();
+            if (!isNaN(focusedPos) && focusedPos >= 0 && focusedPos < size) {
+                var h = this._getItemHeight();
+                scrollPos = focusedPos * h;
+            }
+            this._focusedItemIdx = focusedPos;
+            this._setScrollPos(scrollPos, true);
+        },
+        /**
+         * Adjust the absolute position of the items block to reflect exactly
+         * the position of the scroller. This method is required because real
+         * items sizes could be different from the average size returned by the
+         * "_getItemHeight" method.
+         */
+        _adjustPosition : function() {
+            var that = this;
+            var container = that.getDOMNode();
+            var scrollPos = container.scrollTop;
+
+            var h = that._getItemHeight();
+            var pos = scrollPos - that.state.index * h;
+            var height = that.state.items.length * h;
+            var blockNode = that.refs.block.getDOMNode();
+            var blockHeight = blockNode.offsetHeight;
+
+            var blockPos = scrollPos - blockHeight * (pos / height);
+            blockPos = Math.round(blockPos);
+            blockNode.style.top = blockPos + 'px';
+
+            if (!isNaN(that._focusedItemIdx)) {
+                delete that._focusedItemIdx;
+                setTimeout(function() {
+                    container.scrollTop = that.state.scrollPos;
+                }, 30);
+            }
+        },
+        /**
+         * Sets the scroller in the specified position and updates the internal
+         * state if the content should be re-loaded.
+         */
+        _setScrollPos : function(scrollPos, force) {
+            var that = this;
+            var pageSize = that._getPageSize();
+            var h = that._getItemHeight();
+            var container = that.getDOMNode();
+
+            var windowHeight = container.offsetHeight;
+            var delta = windowHeight / 4;
+
+            var startPos = Math.max(scrollPos - delta, 0);
+            var endPos = scrollPos + windowHeight + delta;
+
+            var startItem = Math.floor(startPos / h);
+            var endItem = Math.ceil((endPos + h - 1) / h);
+
+            var startPage = Math.floor(startItem / pageSize);
+            var endPage = Math.ceil((endItem + pageSize - 1) / pageSize);
+
+            var index = startPage * pageSize;
+            var length = (endPage - startPage) * pageSize;
+            var params = {
+                scrollPos : scrollPos,
+                index : index,
+                length : length
+            };
+            if (force || params.index != that.state.index
+                    || params.length != that.state.length) {
+                params.callback = function(items) {
+                    params.items = items;
+                    that.setState(that._newState(params));
+                }
+                that._loadItems(params);
+            } else {
+                that._adjustPosition();
+            }
+        },
+        /** This handler is called when the scroller changes its position. */
+        _onScroll : function(event) {
+            this._setScrollPos(this.getDOMNode().scrollTop);
+        },
+
     });
 
 });
